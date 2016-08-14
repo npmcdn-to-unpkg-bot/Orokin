ca56b3223a07d6a84f9a96ed97e2de52bfcbde25
Aymeric <aymeric.legros@adixon.fr>
2016-08-15 00:48:52 +0200

    Mise à jour Alerts
    Ajout des Json

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -9,14 +9,15 @@
 namespace App\Controller;
 
 use App\Controller;
-use Cake\Cache\Cache;
-use Cake\Event\Event;
+//use Cake\Cache\Cache;
+//use Cake\Event\Event;
 
 class HomeController extends AppController
 {
     public function index()
     {
-
+        $alljson = json_decode(file_get_contents('http://content.warframe.com/dynamic/worldState.php'));
+        $this->set('datas', $alljson);
     }
 
     public function warjson()
@@ -24,18 +25,68 @@ class HomeController extends AppController
         $time = $this->request->data['time'];
         $time = intval($time);
 
+        //TROP DANGEREUX!!!
+        //$allimgjson = json_decode(file_get_contents('http://content.warframe.com/MobileExport/Manifest/ExportManifest.json'));
+
         //On récupère toutes les informations du flux de warframe
-        $json = json_decode(file_get_contents('http://content.warframe.com/dynamic/worldState.php'));
+        $alljson = json_decode(file_get_contents('http://content.warframe.com/dynamic/worldState.php'));
+        $nodesjson = json_decode(file_get_contents('./json/solNodes.json'));
+        $typesjson = json_decode(file_get_contents('./json/missionTypes.json'));
+        $factionjson = json_decode(file_get_contents('./json/factionsData.json'));
+        $rewardjson = json_decode(file_get_contents('./json/rewardAlerts.json'));
+
 
         //Alerts
-        foreach ($json->{'Alerts'} as $key=>$value){
-            $json->{'Alerts'}[$key]->{'Activation'}->{'sec'} = $json->{'Alerts'}[$key]->{'Activation'}->{'sec'} + $time;
-            $json->{'Alerts'}[$key]->{'Activation'}->{'usec'} = date("Y-m-d H:i:s", $json->{'Alerts'}[$key]->{'Activation'}->{'sec'});
-            $json->{'Alerts'}[$key]->{'Expiry'}->{'sec'} = $json->{'Alerts'}[$key]->{'Expiry'}->{'sec'} + $time;
-            $json->{'Alerts'}[$key]->{'Expiry'}->{'usec'} = date("Y-m-d H:i:s", $json->{'Alerts'}[$key]->{'Expiry'}->{'sec'});
+        foreach ($alljson->{'Alerts'} as $value) {
+            $value->{'Activation'}->{'sec'} = $value->{'Activation'}->{'sec'} + $time;
+            $value->{'Activation'}->{'usec'} = date("Y-m-d H:i:s", $value->{'Activation'}->{'sec'});
+            $value->{'Expiry'}->{'sec'} = $value->{'Expiry'}->{'sec'} + $time;
+            $value->{'Expiry'}->{'usec'} = date("Y-m-d H:i:s", $value->{'Expiry'}->{'sec'});
+
+            //Change le nom de la mission
+            foreach ($nodesjson as $key => $node) {
+                if ($value->{'MissionInfo'}->{'location'} == $key) {
+                    $value->{'MissionInfo'}->{'location'} = $node->{'value'};
+                }
+            }
+
+            //Change le type de la mission
+            foreach ($typesjson as $key => $type) {
+                if ($value->{'MissionInfo'}->{'missionType'} == $key) {
+                    $value->{'MissionInfo'}->{'missionType'} = $type->{'value'};
+                }
+            }
+
+            //Change la faction de la mission
+            foreach ($factionjson as $key => $faction) {
+                if ($value->{'MissionInfo'}->{'faction'} == $key) {
+                    $value->{'MissionInfo'}->{'faction'} = $faction->{'value'};
+                }
+            }
+
+            //Change le nom des rewards
+            if (isset($value->{'MissionInfo'}->{'missionReward'}->{'countedItems'})){
+                foreach ($value->{'MissionInfo'}->{'missionReward'}->{'countedItems'} as $value2) {
+                    foreach ($rewardjson as $key2 => $reward) {
+                        if ($value2->{'ItemType'} == $key2) {
+                            $value2->{'ItemType'} = $reward->{'value'};
+                        }
+                    }
+                }
+            }
+
+            if (isset($value->{'MissionInfo'}->{'missionReward'}->{'items'})){
+                foreach ($value->{'MissionInfo'}->{'missionReward'}->{'items'} as $value3) {
+                    foreach ($rewardjson as $key3 => $reward) {
+                        if ($value3 == $key3) {
+                            $value3 = $reward->{'value'};
+                        }
+                    }
+                }
+            }
         }
 
-        $this->set('datas', $json);
+        $this->set('datas', $alljson);
         $this->set('timenow', time() + $time);
 
         //Mise en cache des données
